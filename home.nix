03f709893a66631a5b34cf75f85fd1821ef2aabe
CRBroughton <crbroughton@posteo.uk>
2026-02-13 08:17:34 +0000

    enable experimental flake feature by default

--- home.nix
+++ home.nix
@@ -1,11 +1,20 @@
-{ config, pkgs, fish-flake, podman-flake, ... }:
+{
+  config,
+  pkgs,
+  fish-flake,
+  podman-flake,
+  ...
+}:
 
 {
   home.stateVersion = "25.11";
-
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   programs.fish-shell.enable = true;
   programs.podman-config = {
-   enable = true;
+    enable = true;
   };
 }
