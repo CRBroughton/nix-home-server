324f4d137b7f15ab1c1cc398adaf2bc1cc6b862c
CRBroughton <crbroughton@posteo.uk>
2026-02-15 15:42:54 +0000

    add podman systemd service

--- home.nix
+++ home.nix
@@ -17,4 +17,22 @@
   programs.podman-config = {
     enable = true;
   };
+
+  systemd.user.services.podman-socket-restart = {
+    Unit = {
+      Description = "Restart podman socket on boot";
+      After = [ "default.target" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.systemd}/bin/systemctl --user restart podman.socket";
+      RemainAfterExit = true;
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+  systemd.user.services.podman = {
+    Service = {
+      Environment = "PATH=/run/wrappers/bin:/run/current-system/sw/bin";
+    };
+  };
 }
