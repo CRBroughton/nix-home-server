23cc2b06940b83d2700a481ffd753e6eec996183
CRBroughton <crbroughton@posteo.uk>
2026-02-21 11:58:12 +0000

    remove the dish config
    turns out this conflicts with the existing configuration; it already handles the partitioning

--- pi-nixos/flake.nix
+++ pi-nixos/flake.nix
@@ -1,19 +1,13 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, disko, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.pi = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
       modules = [
         "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        disko.nixosModules.disko
-        ./disk-config.nix
         ./configuration.nix
       ];
     };
